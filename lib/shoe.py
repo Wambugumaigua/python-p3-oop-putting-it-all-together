22e7918c8ac6edd4fbe3ef1189c9a5e4e06d4fbf
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-05-29 01:39:13 -0700

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+
 class Shoe:
-    pass
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.is_worn = False
+
+    def wear(self):
+        self.is_worn = True
+
+    def clean(self):
+        self.is_worn = False
